d482abec70497962eebe6e63ae2f5fcaba046657
Kamil Makoś <makos.kamil@o2.pl>
2018-07-15 21:14:55 +0200

    fix_css_add_js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-var span = $('span');
-    span.each(function(index, element) {
-	if(index % 2 == 0) {
-		$(element).css('color', 'red');
-	};
-});
-
-var paragraphs = $('p');
-	paragraphs.each(function(index, element) {
-
-	var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
-		$(element).append(button);
-});
-
-$('button').click(function(){
-	alert($(this).attr('data-tmp'));
+$(function(){
+	var carouselList = $("#carousel ul");
+	
+	function moveFirstSlide () {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		
+		$(lastItem).after(firstItem);
+		$(carouselList).css('margin-left', '0');
+	}
+		function changeSlide() {
+		carouselList.animate({'marginLeft': -400}, 500, moveFirstSlide);	
+	}
+	
+	setInterval(changeSlide, 3000);
+	
 });
